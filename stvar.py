1827c15b60bce7c703d63a5f406256b6b6667022
AnaGalic <49649776+AnaGalic@users.noreply.github.com>
2020-04-02 21:58:03 +0200

    Update stvar.py

--- stvar.py
+++ stvar.py
@@ -9,3 +9,14 @@ class Stvar(object):
         Stvar.brStvari -= 1
 
   
+print('*** test 1 ***')
+s1 = Stvar()
+s2 = Stvar()
+s3 = s2
+print(Stvar.broj_stvari)
+del(s2)
+print(Stvar.broj_stvari)
+del(s3)
+print(Stvar.broj_stvari)
+del(s1)
+print(Stvar.broj_stvari)
