adf080b50e9cdd0f6cbbd90b6c527c03f47f1fe8
AnaGalic <49649776+AnaGalic@users.noreply.github.com>
2020-04-30 21:43:09 +0200

    Update funkcije.py

--- Zadaca5/funkcije.py
+++ Zadaca5/funkcije.py
@@ -1,8 +1,22 @@
-import likovi, funkcije
+import likovi
+import math
+from math import pi
 
-k1 = likovi.Kruznica(3)
-k2 = likovi.Kvadrat(5)
 
-print('*** test program ***')
-print(k1, 'opsega', funkcije.opseg(k1), 'povrsine', funkcije.povrsina(k1))
-print(k2, 'opsega', funkcije.opseg(k2), 'povrsine', funkcije.povrsina(k2))
+def opseg(lik):
+    if isinstance(lik, likovi.Kvadrat):
+        return 4 * lik.duljinaStranice
+    elif isinstance(lik,  likovi.Kruznica):
+        return 2 * lik.radijus * pi
+
+
+def povrsina(lik):
+    if isinstance(lik, likovi.Kvadrat):
+        return pow(lik.duljinaStranice, 2)
+    elif isinstance(lik,  likovi.Kruznica):
+        return pow(lik.radijus, 2) * pi
+
+if __name__ == "__main__":
+    print('*** test funkcije ***')
+    print(opseg.__name__)
+    print(povrsina.__name__)
